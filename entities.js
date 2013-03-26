059f49e316e5ba3f527375f9a38ca0fb9a516726
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-03-26 21:02:33 +0100

    EnemyEntity, kolli entity objekt

--- entities.js
+++ entities.js
@@ -67,3 +67,77 @@ var PlayerEntity = me.ObjectEntity.extend({
     }
  
 });
+
+/* --------------------------
+an enemy Entity
+------------------------ */
+var EnemyEntity = me.ObjectEntity.extend({
+    init: function(x, y, settings) {
+        // define this here instead of tiled
+        settings.image = "wheelie_right";
+        settings.spritewidth = 64;
+
+        // call the parent constructor
+        this.parent(x, y, settings);
+
+        this.startX = x;
+        this.endX = x + settings.width - settings.spritewidth;
+        // size of sprite
+
+        // make him start from the right
+        this.pos.x = x + settings.width - settings.spritewidth;
+        this.walkLeft = true;
+
+        // walking & jumping speed
+        this.setVelocity(4, 6);
+
+        // make it collidable
+        this.collidable = true;
+        // make it a enemy object
+        this.type = me.game.ENEMY_OBJECT;
+
+    },
+
+    // call by the engine when colliding with another object
+    // obj parameter corresponds to the other object (typically the player) touching this one
+    onCollision: function(res, obj) {
+
+        // res.y >0 means touched by something on the bottom
+        // which mean at top position for this one
+        if (this.alive && (res.y > 0) && obj.falling) {
+            this.flicker(45);
+        }
+    },
+
+    // manage the enemy movement
+    update: function() {
+        // do nothing if not visible
+        if (!this.visible)
+            return false;
+
+        if (this.alive) {
+            if (this.walkLeft && this.pos.x <= this.startX) {
+                this.walkLeft = false;
+            } else if (!this.walkLeft && this.pos.x >= this.endX) {
+                this.walkLeft = true;
+            }
+            // make it walk
+            this.flipX(this.walkLeft);
+            this.vel.x += (this.walkLeft) ? -this.accel.x * me.timer.tick : this.accel.x * me.timer.tick;
+
+        } else {
+            this.vel.x = 0;
+        }
+
+        // check and update movement
+        this.updateMovement();
+
+        // update animation if necessary
+        if (this.vel.x !== 0 || this.vel.y !== 0) {
+            // update object animation
+            this.parent();
+            return true;
+        }
+        return false;
+    }
+});
