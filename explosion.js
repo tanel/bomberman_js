f2ffe93ada36a5f6fbf4016e1cfbc56bc8226fd2
SiimGitHub <siimeestist@gmail.com>
2013-04-09 14:18:11 +0300

    explosion.js (muudatus)
    Muudetud collision koodi (kui vastane saab plahvatusega tabamuse, siis lõpetab ta liikumise).

--- explosion.js
+++ explosion.js
@@ -21,6 +21,25 @@ var Explosion = me.ObjectEntity.extend({
         if (mres && mres.length > 0) {
             console.dir(mres);
         }
+        
+        var res2 = me.game.collide(this);
+    	
+        if (res2) {
+            // if we collide with the bomb
+            if (res2.obj.type === me.game.ENEMY_OBJECT) {
+                console.log('collision with enemy');
+                // Pommi puudutamine paneb flickerdama
+                //this.flicker(45);
+                //res2.obj.flicker(45);
+                res2.obj.alive = false; // Vastase objekt
+            } else if (res2.obj.type === me.game.ACTION_OBJECT) {
+                console.log('collision with action object');
+                //this.flicker(45);
+            } else {
+                console.log('collision with unknown object :)');
+                // FIXME: powerupi puudumine peaks selle üles korjama ning powerupi sisse lülitama
+            }
+        }
 
         if (this.explodeAt < me.timer.getTime()) {
             me.game.remove(this);
