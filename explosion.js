5756165214006d9e6f2fdfd755ebdfd9481bce0e
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-04-11 17:08:35 +0300

    res2 muutuja renametud

--- explosion.js
+++ explosion.js
@@ -19,24 +19,16 @@ var Explosion = me.ObjectEntity.extend({
         this.updateColRect(-16*this.bombRadius + 16, 32*this.bombRadius, -1); // Selgitus: mäng ühtlaselt jaotab laienemist saates algpunkti 16p tagasi
         // Otsime entiteete, mis jäävad plahvatuse alasse.                       ja kokku laiendades 32p. +16p on vaja selleks, et esimese ruuduga
         // Leitud entiteedid võib nö sodiks lasta.                               algpunkti tagasisaatmist ei toimuks.
-        var mres = me.game.collide(this, true);
-        if (mres && mres.length > 0) {
-            console.dir(mres);
-        }
         
-        var res2 = me.game.collide(this);
+        var res = me.game.collide(this);
     	
-        if (res2) {
+        if (res) {
             // if we collide with the bomb
-            if (res2.obj.type === me.game.ENEMY_OBJECT) {
+            if (res.obj.type === me.game.ENEMY_OBJECT) {
                 console.log('collision with enemy');
-                // Pommi puudutamine paneb flickerdama
-                //this.flicker(45);
-                //res2.obj.flicker(45);
-                res2.obj.alive = false; // Vastase objekt
-            } else if (res2.obj.type === me.game.ACTION_OBJECT) {
+                res.obj.alive = false; // Vastase objekt
+            } else if (res.obj.type === me.game.ACTION_OBJECT) {
                 console.log('collision with action object');
-                //this.flicker(45);
             } else {
                 console.log('collision with unknown object :)');
                 // FIXME: powerupi puudumine peaks selle üles korjama ning powerupi sisse lülitama
