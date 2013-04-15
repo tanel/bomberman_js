65774e8474453c4a6bd7cbe83e03c01d50389b82
Osaar <minumegamail@gmail.com>
2013-04-15 17:37:08 +0300

    mäng töötab edasi kui ogg põhjustab probleeme

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 (function () {
     "use strict";
 
-    //me.debug.renderHitBox = true;
+    me.debug.renderHitBox = true;
 
     // bomberman on nö namespace, mille küljes asuvad
     // kõik mänguga seotud objektid, funktsioonid, ressursid jne.
@@ -69,8 +69,16 @@
             // MelonJS loader proovib audiofaile laadida sellest järjekorras: mp3, ogg.
             // Kui veebilehitsejal puuduvad sobivad audio codec'id, siis mängul heli puudub
             // (vt http://www.melonjs.org/docs/symbols/me.audio.html#init)
-            me.audio.init("mp3,ogg");
-
+            try 
+		{
+                var start = me.audio.init("ogg").value;
+		if (x==false)    throw "Loading ogg failed!";
+	        }
+	    catch(err)
+		{
+		console.log("there was an error loading audio in ogg format!")
+		me.audio.init("mp3");
+		}
             // loader tegeleb mängu ressursside laadimisega. Kui ta on laadimisega
             // valmis saanud, siis tahame, et ta käivitaks meie funktsiooni "loaded":
             // (vt http://www.melonjs.org/docs/symbols/me.loader.html)
