c6a53487ee4ad8eedd59fce66cd0ff79a80efb6a
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-04-18 22:35:51 +0300

    Plahvatuse laiendamist lihtsamaks

--- explosion.js
+++ explosion.js
@@ -1,5 +1,5 @@
 var Explosion = me.ObjectEntity.extend({
-    init: function(x, y, settings, dir) {
+    init: function(x, y, settings, direction) {
         settings.image = "boom";
         me.audio.play("bomb", false, null, 0.6);
         settings.spritewidth = 32;
@@ -13,25 +13,7 @@ var Explosion = me.ObjectEntity.extend({
         this.extTime = 70; // Laienemisaeg
         this.bombRadius = 6;
         this.currentRadius = 1; // Hetkel kui palju plahvatus laienend on
-        this.dir = dir;
-        this.laiendusX = -1;
-        this.laiendusY = -1;
-        switch (dir) {
-        case "left":
-            this.laiendusX = -32; 
-            break;
-        case "right":
-            this.laiendusX = 32; 
-            break;
-        case "up":
-            this.laiendusY = 32; 
-            break;
-        case "down":
-            this.laiendusY = -32; 
-            break;
-        default:
-            throw("Explosion direction Unknown!");
-        }
+        this.direction = direction;
 	
         // Ajaarvamise algus :)
         this.startMoment = me.timer.getTime();
@@ -45,12 +27,14 @@ var Explosion = me.ObjectEntity.extend({
         if (! this.visible)
             return false;
 
-        // Collisionboxi laiendamine ajaliselt
-        if ((this.startMoment + this.extTime * this.currentRadius) <= me.timer.getTime()) {
-            this.grow();
+        // * this.currentRadius
+        console.log(this.currentRadius);
+        if (this.currentRadius < this.bombRadius) {
+            if (this.direction === "right") {
+                this.updateColRect(-1, 32 * this.currentRadius, -1, 32);
+            }
+            this.currentRadius++;
         }
-
-        var viewNeeedsUpdate = false;
 
         // Otsime entiteete, mis jäävad plahvatuse alasse.                       
         // Leitud entiteedid võib nö sodiks lasta.
@@ -64,31 +48,17 @@ var Explosion = me.ObjectEntity.extend({
                 row = parseInt((res.x + this.pos.x) / 32, 10);
                 col = parseInt((res.y + this.pos.y) / 32, 10);
                 me.game.currentLevel.clearTile(row, col);
-                viewNeeedsUpdate = true;
             } else if (res.obj.type === me.game.ACTION_OBJECT) {
                 row = Math.round(res.x / 32);
                 col = Math.round(res.y / 32);
                 me.game.currentLevel.clearTile(row, col);
-                viewNeeedsUpdate = true;
             }
         }
 
         if (this.endTime <= me.timer.getTime()) {
             me.game.remove(this);
-            viewNeeedsUpdate = true;
         }
 
-        return viewNeeedsUpdate;
-    },
-
-    grow: function() {
-        var w = this.laiendusX * this.currentRadius;
-        var h = this.laiendusY * this.currentRadius;
-        console.log('w = ', w, 'h = ', h);
-
-        this.updateColRect(-1, w,-1, h);
-        if (this.currentRadius < this.bombRadius) {
-            this.currentRadius++;
-        } 
-    } 
+        return true;
+    }
 });
