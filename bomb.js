b17712afdb638138ff1c6fc5df4c12b1e265a9b8
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-04-18 22:47:21 +0300

    Plahvatuse tükkidele lihtsamad nimed ;)

--- bomb.js
+++ bomb.js
@@ -43,14 +43,14 @@ var BombEntity = me.ObjectEntity.extend({
         if (this.explodeAt < me.timer.getTime()) {
             this.player.bombs = this.player.bombs - 1;
             var settings = {bomb: this};
-            var arkRight = new Explosion(this.pos.x, this.pos.y, settings, "right");
-            var arkLeft = new Explosion(this.pos.x, this.pos.y, settings, "left");
-            var arkUp = new Explosion(this.pos.x, this.pos.y, settings, "up");
-            var arkDown = new Explosion(this.pos.x, this.pos.y, settings, "down");
-            me.game.add(arkRight, 1000);
-            me.game.add(arkLeft, 1000);
-            me.game.add(arkUp, 1000);
-            me.game.add(arkDown, 1000);
+            var explodeRight = new Explosion(this.pos.x, this.pos.y, settings, "right");
+            var explodeLeft = new Explosion(this.pos.x, this.pos.y, settings, "left");
+            var explodeUp = new Explosion(this.pos.x, this.pos.y, settings, "up");
+            var explodeDown = new Explosion(this.pos.x, this.pos.y, settings, "down");
+            me.game.add(explodeRight, 1000);
+            me.game.add(explodeLeft, 1000);
+            me.game.add(explodeUp, 1000);
+            me.game.add(explodeDown, 1000);
             me.game.sort();
             me.game.remove(this);
             this.parent();
