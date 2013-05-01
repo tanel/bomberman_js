c8129c8b8f176b96a27e23a37d5c8f3a2bdceda1
Osaar <minumegamail@gmail.com>
2013-05-01 21:13:05 +0300

    screens.js template

---
+++ screens.js
@@ -0,0 +1,29 @@
+/*----------------------
+
+    A title screen
+
+    ----------------------*/
+
+var TitleScreen = me.ScreenObject.extend({
+    // constructor
+    init: function() {
+        this.parent(true);
+    },
+
+    // reset function
+    onResetEvent: function() {
+	},
+
+    // update function
+    update: function() {
+	},
+
+    // draw function
+    draw: function(context) {
+	},
+
+    // destroy function
+    onDestroyEvent: function() {
+	}
+
+}); 
