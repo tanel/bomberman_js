1872fb6f5bcf2151497b5591e0f8afae863a5d21
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-05-12 16:23:25 +0300

    Render instructions by default

--- screens.js
+++ screens.js
@@ -32,7 +32,6 @@ var TitleScreen = me.ScreenObject.extend({
         this.scrollerfont = null;
         this.scrollertween = null;
         this.scrollerpos = 320;
-        this.instruction = null;
     },
 
     onResetEvent: function() {
@@ -45,24 +44,17 @@ var TitleScreen = me.ScreenObject.extend({
             // set the scroller
             this.scrollerfont = new me.BitmapFont("32x32_font", 32);
             this.scrollerfont.set("left");
-            this.instruction = false;
         }
         // reset to default value
         this.scrollerpos = 320;
 
         // enable the keyboard
         me.input.bindKey(me.input.KEY.ENTER, "enter", true);
-        me.input.bindKey(me.input.KEY.V, "instructions", true);
     },
 
     update: function() {
         if (me.input.isKeyPressed('enter')) {
             me.state.change(me.state.PLAY);
-            return true;
-        }
-        if (me.input.isKeyPressed('instructions')) {
-            this.instruction = true;
-            console.log("Noob detected!");
             return true;
         }
         return false;
@@ -72,13 +64,11 @@ var TitleScreen = me.ScreenObject.extend({
     draw: function(context) {
         context.drawImage(this.title, 0, 425);
         this.font.draw(context, "PRESS ENTER TO PLAY", 400, 300);
-        this.font.draw(context, "PRESS V FOR GAME INSTRUCTIONS", 400, 325);
         this.titlefont.draw(context, "BOMBERMAN", 400, 25);
-        if (this.instruction) {
-            this.font.draw(context, "YOUR MISSION IS TO KILL EVERYONE EXCEPT YOURSELF.", 400, 75);
-            this.font.draw(context, "USE ARROWS TO MOVE AND X TO PLANT BOMB.", 400, 100);
-            this.font.draw(context, "HAPPY HUNTING! AND TRY NOT TO BLOW YOURSELF UP...", 400, 125);
-        }
+
+        this.font.draw(context, "YOUR MISSION IS TO KILL EVERYONE EXCEPT YOURSELF.", 400, 75);
+        this.font.draw(context, "USE ARROWS TO MOVE AND X TO PLANT BOMB.", 400, 100);
+        this.font.draw(context, "HAPPY HUNTING! AND TRY NOT TO BLOW YOURSELF UP...", 400, 125);
     },
 
     onDestroyEvent: function() {
