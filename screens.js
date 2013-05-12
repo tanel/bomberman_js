fee34d5ff1b2dab73b28778a0b1090aa5aa06df4
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-05-12 20:34:25 +0300

    JS linted

--- screens.js
+++ screens.js
@@ -35,8 +35,8 @@ var TitleScreen = me.ScreenObject.extend({
         // title screen image
         this.title = null;
 	
-	this.instructions = false;
-	this.score = false;
+        this.instructions = false;
+        this.score = false;
 
         // font to display the menu items
         this.titlefont = new me.Font("Cursive", 40, "yellow", "center");
@@ -51,27 +51,24 @@ var TitleScreen = me.ScreenObject.extend({
 
         // enable the keyboard
         me.input.bindKey(me.input.KEY.ENTER, "enter", true);
-	me.input.bindKey(me.input.KEY.V, "instructions", true);
-	me.input.bindKey(me.input.KEY.S, "score", true);
+        me.input.bindKey(me.input.KEY.V, "instructions", true);
+        me.input.bindKey(me.input.KEY.S, "score", true);
     },
 
     update: function() {
         if (me.input.isKeyPressed('enter')) {
             me.state.change(me.state.PLAY);
             return true;
-        }
-        if (me.input.isKeyPressed('instructions')) {
-	    this.instructions = true;
-	    this.score = false;
-	    console.log("Noob detected!");
-	    return true;
-	
-        }
-        if (me.input.isKeyPressed('score')) {
-	    this.score = true;
-	    this.instructions = false;
-	    console.log("Wannabe pro detected!"); 
-	    return true;
+        } else if (me.input.isKeyPressed('instructions')) {
+            this.instructions = true;
+            this.score = false;
+            console.log("Noob detected!");
+            return true;
+        } else if (me.input.isKeyPressed('score')) {
+            this.score = true;
+            this.instructions = false;
+            console.log("Wannabe pro detected!"); 
+            return true;
         } 
         return false;
     },
@@ -80,15 +77,14 @@ var TitleScreen = me.ScreenObject.extend({
     draw: function(context) {
         context.drawImage(this.title, 0, 425);
         this.titlefont.draw(context, "BOMBERMAN", 400, 50);
-	this.font.draw(context, "PRESS ENTER TO PLAY", 400, 300);
+        this.font.draw(context, "PRESS ENTER TO PLAY", 400, 300);
         if (this.instructions) {
-	    this.font.draw(context, "YOUR MISSION IS TO KILL EVERYONE EXCEPT YOURSELF.", 400, 75);
-	    this.font.draw(context, "USE ARROWS TO MOVE AND X TO PLANT BOMB.", 400, 100);
-	    this.font.draw(context, "HAPPY HUNTING! AND TRY NOT TO BLOW YOURSELF UP...", 400, 125);
-	}
-	else if (this.score) {
-	    this.font.draw(context, "SCORE IS TOTALLY AMAZING: 0 POINTS, WELL DONE!", 400, 100);
-	}
+            this.font.draw(context, "YOUR MISSION IS TO KILL EVERYONE EXCEPT YOURSELF.", 400, 75);
+            this.font.draw(context, "USE ARROWS TO MOVE AND X TO PLANT BOMB.", 400, 100);
+            this.font.draw(context, "HAPPY HUNTING! AND TRY NOT TO BLOW YOURSELF UP...", 400, 125);
+        } else if (this.score) {
+            this.font.draw(context, "SCORE IS TOTALLY AMAZING: 0 POINTS, WELL DONE!", 400, 100);
+        }
     },
 
     onDestroyEvent: function() {
