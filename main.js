55d0c75122d2d95b16d1c66082341108c1000994
Osaar <minumegamail@gmail.com>
2013-05-12 12:16:59 +0000

    mängujuhised menüüsse

--- main.js
+++ main.js
@@ -163,6 +163,7 @@
         this.scrollerfont = null;
         this.scrollertween = null;
 	this.scrollerpos = 320;
+	this.instruction = null;
     },
 
     // reset function
@@ -176,12 +177,16 @@
             // set the scroller
             this.scrollerfont = new me.BitmapFont("32x32_font", 32);
             this.scrollerfont.set("left");
+	    this.instruction = false;
+	    
+	    
 	}
 	// reset to default value
         this.scrollerpos = 320;
 	
 	// enable the keyboard
         me.input.bindKey(me.input.KEY.ENTER, "enter", true);
+	me.input.bindKey(me.input.KEY.V, "instructions", true);
     },
 
     // update function
@@ -190,6 +195,10 @@
         if (me.input.isKeyPressed('enter')) {
             me.state.change(me.state.PLAY);
         }
+	if (me.input.isKeyPressed('instructions')) {
+	    this.instruction = true;
+	    console.log("Noob detected!");
+        }
         return true;
     },
 
@@ -197,13 +206,19 @@
     draw: function(context) {
 	context.drawImage(this.title, 0, 425);
 	this.font.draw(context, "PRESS ENTER TO PLAY", 400, 300);
-	this.font.draw(context, "PRESS X FOR GAME INSTRUCTIONS", 400, 325);
-	this.titlefont.draw(context, "Bomberman", 400, 25);
+	this.font.draw(context, "PRESS V FOR GAME INSTRUCTIONS", 400, 325);
+	this.titlefont.draw(context, "BOMBERMAN", 400, 25);
+	if (this.instruction === true) {
+	    this.font.draw(context, "YOUR MISSION IS TO KILL EVERYONE EXCEPT YOURSELF.", 400, 75);
+	    this.font.draw(context, "USE ARROWS TO MOVE AND X TO PLANT BOMB.", 400, 100);
+	    this.font.draw(context, "HAPPY HUNTING! AND TRY NOT TO BLOW YOURSELF UP...", 400, 125);
+	}
     },
 
     // destroy function
     onDestroyEvent: function() {
 	me.input.unbindKey(me.input.KEY.ENTER);
+	me.input.unbindKey(me.input.KEY.V);
     }
 
     }); 
