ed5414a9a5ed209aed2fd4def5b40ba6dbd7dfd2
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-05-14 23:55:55 +0300

    Don't add score and lives on each reset

--- play_screen.js
+++ play_screen.js
@@ -4,6 +4,9 @@
 // (vt http://www.melonjs.org/docs/symbols/me.ScreenObject.html)
 var PlayScreen = me.ScreenObject.extend({
 
+    score: null,
+    lives: null,
+
     // constructor
     init: function(settings) {
         this.parent(true);
@@ -16,9 +19,15 @@ var PlayScreen = me.ScreenObject.extend({
         me.levelDirector.loadLevel("level1");
 
         // add the "score" HUD item
-        me.game.HUD.addItem("score", new ScoreObject(10, 40));
+        if (!this.score) {
+            this.score = new ScoreObject(10, 40);
+            me.game.HUD.addItem("score", this.score);
+        }
 
         // add the "lives" HUD item
-        me.game.HUD.addItem("lives", new LivesObject(me.video.getWidth() - 200, 40));
+        if (!this.lives) {
+            this.lives = new LivesObject(me.video.getWidth() - 200, 40);
+            me.game.HUD.addItem("lives", this.lives);
+        }
     }
 });
