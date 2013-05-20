815f2b1ea4eb1b792d85f54fcc14be1f9a7ef08c
Osaar <minumegamail@gmail.com>
2013-05-20 05:58:04 +0000

    fix

--- explosion.js
+++ explosion.js
@@ -114,12 +114,13 @@ var Explosion = me.ObjectEntity.extend({
     },
     foundItem: function(pixelCoords) {
         // To-do: this method should contain what to drop when wall is destroyed and at which frequency
-	if (Math.random() > 0.7) {
+        var roll = Math.random();
+	if (roll > 0.7) {
 	    var coin = new CoinEntity(pixelCoords.x, pixelCoords.y);
 	    me.game.add(coin, 1000);
 	    me.game.sort();
 	}
-	else if (Math.random() < 0.1) {
+	else if (roll < 0.1) {
 	    var flamepower = new FlamePowerEntity(pixelCoords.x, pixelCoords.y);
 	    me.game.add(flamepower, 1000);
 	    me.game.sort();
