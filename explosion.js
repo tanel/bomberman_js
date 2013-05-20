db2823e996af089723d7d7c6a02566ad8f00c83c
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-05-20 23:54:05 +0300

    Explosion expands

--- explosion.js
+++ explosion.js
@@ -1,6 +1,10 @@
 var Explosion = me.ObjectEntity.extend({
-    init: function(x, y) {
+    init: function(x, y, bombRadius, maxBombRadius, direction) {
         me.audio.play("bomb", false, null, 0.6);
+
+        this.currentBombRadius = bombRadius || 1;
+        this.maxBombRadius = maxBombRadius || me.game.HUD.getItemValue("range");
+        this.currentDirection = direction || new me.Vector2d(0, 0);
 
         var settings = {
             image: "boom",
@@ -12,8 +16,6 @@ var Explosion = me.ObjectEntity.extend({
         };
 
         this.parent(x, y, settings);
-
-        this.bombRadius = me.game.HUD.getItemValue("range");
 
         // Timer when explosion is hidden
         this.clearAt = me.timer.getTime() + 0.2 * 1000;
@@ -41,12 +43,33 @@ var Explosion = me.ObjectEntity.extend({
 
         if (this.clearAt < me.timer.getTime()) {
             window.bomberman.clearBreakingTiles(this.left, this.right, this.top, this.bottom);
+            if (this.currentBombRadius <= this.maxBombRadius) {
+                // In first stage, expand in all 4 directions.
+                if (this.currentBombRadius === 1) {
+                    this.expand(new me.Vector2d(0, -1)); // one row up
+                    this.expand(new me.Vector2d(0, +1)); // one row down
+                    this.expand(new me.Vector2d(-1, 0)); // one col left
+                    this.expand(new me.Vector2d(+1, 0)); // one col right
+                } else {
+                    // After initial expansion in all 4 directions, continue
+                    // expanding in the current direction.
+                    this.expand(this.currentDirection);
+                }
+            }
             this.visible = false;
             me.game.remove(this);
+            me.game.sort();
         }
 
-        // update object animation
-        this.parent();
-        return true;
+        return false;
+    },
+
+    expand: function(direction) {
+        var tileCoords = window.bomberman.pixelToTileCoords(this.pos.x, this.pos.y);
+        var col = tileCoords.x;
+        var row = tileCoords.y;
+        var coords = window.bomberman.tileToPixelCoords(col + direction.x, row + direction.y);
+        var explosion = new Explosion(coords.x, coords.y, this.currentBombRadius + 1, this.maxBombRadius, direction);
+        me.game.add(explosion, 1000);
     }
 });
