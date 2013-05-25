4b2ae2548c5fd9985c33b12dafbe5e710b09f805
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-05-25 16:44:37 +0300

    Fmt

--- score_hud_item.js
+++ score_hud_item.js
@@ -1,12 +1,12 @@
 // See http://melonjs.github.io/docs/me.HUD_Object.html
 ScoreObject = me.HUD_Item.extend({
 
-   init: function(x, y) {
-      this.parent(x, y);
-      this.font = new me.Font("Cursive", 30, "yellow");
-   },
+	init: function(x, y) {
+		this.parent(x, y);
+		this.font = new me.Font("Cursive", 30, "yellow");
+	},
 
-   draw: function (context, x, y) {
-      this.font.draw(context, "Score: " + this.value, this.pos.x + x, this.pos.y + y);
-   }
+	draw: function (context, x, y) {
+		this.font.draw(context, "Score: " + this.value, this.pos.x + x, this.pos.y + y);
+	}
 });
