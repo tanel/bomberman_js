3c2cf8372596cdee7c5b3c873f8651f5bb482208
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-05-25 16:43:21 +0300

    Rename HUD items

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,22 +3,7 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		jshint: {
 			files: [
-				'bomb.js',
-				'coin_entity.js',
-				'enemy.js',
-				'explosion.js',
-                'flame_power_entity.js',
-				'gruntfile.js',
-				'help_screen.js',
-				'life_powerup_entity.js',
-				'lives_object.js',
-				'main.js',
-				'play_screen.js',
-				'player.js',
-                'range_object.js',
-				'score_object.js',
-				'score_screen.js',
-				'title_screen.js'
+				'*.js'
 			]
 		}
 	});
