972aa7c3dc6d2530516201bfa446ead76b7c2e8d
Osaar <minumegamail@gmail.com>
2013-05-26 23:42:59 +0000

    some info about powerups

--- help_screen.js
+++ help_screen.js
@@ -9,6 +9,13 @@ var HelpScreen = me.ScreenObject.extend({
     clearInfo: function(context) {
         context.clearRect(0, 55, me.video.getWidth(), 200);
     },
+    onResetEvent: function() {
+        if (!this.life) {
+            // init stuff if not yet done
+            this.life = me.loader.getImage("life");
+	    this.flamepower = me.loader.getImage("flamepower");
+	}
+    },
 
     update: function() {
         if (me.input.isKeyPressed('abort')) {
@@ -20,9 +27,13 @@ var HelpScreen = me.ScreenObject.extend({
 
     draw: function(context) {
         this.clearInfo(context);
+	context.drawImage(this.life, 0, 175);
+	context.drawImage(this.flamepower, 0, 225);
         this.font.draw(context, "YOUR MISSION IS TO KILL EVERYONE EXCEPT YOURSELF.", me.video.getWidth()/2, 100);
         this.font.draw(context, "USE ARROWS TO MOVE AND X TO PLANT BOMB.", me.video.getWidth()/2, 125);
         this.font.draw(context, "HAPPY HUNTING! AND TRY NOT TO BLOW YOURSELF UP...", me.video.getWidth()/2, 150);
+	this.font.draw(context, "THIS POWERUP GIVES YOU LIFE, KEEP A WATCHOUT FOR THIS ONE", me.video.getWidth()/2, 200);
+	this.font.draw(context, "THIS POWERUP MAKES YOUR BOMBS MORE POWERFUL, NICE TO HAVE", me.video.getWidth()/2, 250);
     }
 
 }); 
