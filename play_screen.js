dae90f2b7eadad1536c31efd4b76ce413b7df71b
Tanel Lebedev <tanel.lebedev@gmail.com>
2013-05-28 21:46:38 +0300

    Fix HUD item y scale

--- play_screen.js
+++ play_screen.js
@@ -22,27 +22,29 @@ var PlayScreen = me.ScreenObject.extend({
         me.levelDirector.loadLevel("level1");
 
         // Split HUD into 4 sectors
-        var sectorWidth = me.video.getWidth() / 4, hudItemWidth = 50;
+        var sectorWidth = me.video.getWidth() / 4;
+        var x = 10;
+        var y = 40;
 
         if (!this.score) {
-            this.score = new ScoreObject(0 * sectorWidth, hudItemWidth);
+            this.score = new ScoreObject(0 * sectorWidth + x, y);
             me.game.HUD.addItem("score", this.score);
         }
 
         if (!this.range) {
-            this.range = new RangeObject(1 * sectorWidth, hudItemWidth);
+            this.range = new RangeObject(1 * sectorWidth + x, y);
             me.game.HUD.addItem("range", this.range);
             me.game.HUD.setItemValue("range", 2);
         }
 
         if (!this.lives) {
-            this.lives = new LivesObject(2 * sectorWidth, hudItemWidth);
+            this.lives = new LivesObject(2 * sectorWidth + x, y);
             me.game.HUD.addItem("lives", this.lives);
             me.game.HUD.setItemValue("lives", 3);
         }
 
         if (!this.timeHudItem) {
-            this.timeHudItem = new TimeHudItem(3 * sectorWidth, hudItemWidth);
+            this.timeHudItem = new TimeHudItem(3 * sectorWidth + x, y);
             me.game.HUD.addItem("time", this.timeHudItem);
         }
 
