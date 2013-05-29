dcbe112c1d3e586ac5490acff0a4bb562b9bf906
SiimGitHub <siimeestist@gmail.com>
2013-05-29 17:53:55 +0300

    Uus fail playerName HUD itemi tarbeks
    Kasutatakse mängija nime salvestamisel.

---
+++ name_hud_item.js
@@ -0,0 +1,12 @@
+// See http://melonjs.github.io/docs/me.HUD_Object.html
+NameObject = me.HUD_Item.extend({
+
+   init: function(x, y) {
+      this.parent(x, y);
+      this.font = new me.Font("Cursive", 30, "yellow");
+   },
+
+   draw: function (context, x, y) {
+      //this.font.draw(context, "", this.pos.x + x, this.pos.y + y);
+   }
+});
